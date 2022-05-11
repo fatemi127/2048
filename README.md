e5a6e94189c2442c3d16a620d8190c0cc39eaa31
fatemi127 <fatemi127@gmail.com>
2022-05-12 00:15:09 +0430

    UPDATE! Up,Down,Right and left Keys.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 2048
+
+https://user-images.githubusercontent.com/12674810/167932519-97e5b85c-908d-447d-8a34-c45812c75da0.mp4
+
+
 ![2048](https://user-images.githubusercontent.com/12674810/167296796-38bc9412-9c7c-49c0-bd17-a49ea85f4967.jpg)
--- g2048/Form1.cs
+++ g2048/Form1.cs
@@ -14,6 +14,7 @@ namespace g2048
 
     {
         Label[,] game_board;
+        int n = 4;
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +24,7 @@ namespace g2048
                 for (int j = 0; j < 4; j++)
                 {
                     game_board[i, j] = new Label();
-                    game_board[i, j].Text = "2";
+                    //game_board[i, j].Text = "2";
                     game_board[i, j].Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
                     game_board[i, j].Font = new Font("Tahoma", 32);
                     game_board[i, j].BackColor = Color.Red;
@@ -36,31 +37,100 @@ namespace g2048
                 }
 
             }
+        Random r = new Random();
+            int r1,r2,r1_x,r1_y, r2_x,r2_y,ran_index;
+            int[] init_numbers ={2,2,2,2,2,4};
+            ran_index = r.Next(0, init_numbers.Length);
+            r1 = init_numbers[ran_index];
+            r2 = init_numbers[ran_index];
+
+        r1_x = r.Next(0,3);
+        r1_y = r.Next(0, 3);
+        do
+	{
+        r2_x = r.Next(0,3);
+        r2_y = r.Next(0,3);
+	} while (r1_x ==r2_x &&r1_y ==r2_y);
+
+        game_board[r1_x, r1_y].Text = Convert.ToString(r1);
+        game_board[r2_x, r2_y].Text = Convert.ToString(r2);
+
         }
 
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        
 
-        }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyData == Keys.Up)
             {
-                MessageBox.Show("بالا");
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (j > 0 && game_board[i , j-1].Text == "")
+                            {
+                                game_board[i , j-1].Text = game_board[i, j].Text;
+                                game_board[i, j].Text = "";
+                            }
+                        }
+                    }
+                }
+
             }
             else if (e.KeyData == Keys.Down)
             {
-                MessageBox.Show("پایین");
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (j < n-1 && game_board[i, j + 1].Text == "")
+                            {
+                                game_board[i, j + 1].Text = game_board[i, j].Text;
+                                game_board[i, j].Text = "";
+                            }
+                        }
+                    }
+                }
             }
             else if (e.KeyData == Keys.Left)
             {
-                MessageBox.Show("چپ");
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (i > 0 && game_board[i-1, j ].Text == "")
+                            {
+                                game_board[i-1,j].Text = game_board[i, j].Text;
+                                game_board[i, j].Text = "";
+                            }
+                        }
+                    }
+                }
             }
             else if (e.KeyData == Keys.Right)
             {
-                MessageBox.Show("راست");
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (i < n - 1 && game_board[i+1, j].Text == "")
+                            {
+                                game_board[i+1, j ].Text = game_board[i, j].Text;
+                                game_board[i, j].Text = "";
+                            }
+                        }
+                    }
+                }
             }
         }
     }
